ab8a35e35af5222e13f6eacf94f80efbede9b3fc
brecec <brendaramirez@brendas-mbp.lan>
2023-05-17 21:36:11 -0500

    finalupdates

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # dailyplanner
+
+## Description  
+The daily planner has been created to display a 9-5 work day , broken down by hourly tasks and color coded to represent whether the time block is in the present, past or future.
+
+## Visuals
+![Alt text](assets/Work%20Day%20Scheduler.gif)
+
+## Installation
+
+Additional installation is not required, please follow github link for access: https://bramirez09.github.io/dailyplanner/
+
+## Usage
+
+The daily planner can be used to input task for each hour of the work day, user can refresh the page without losing previously entered data.
--- script.js
+++ script.js
@@ -3,88 +3,68 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-// $(function () {
-// TODO1: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
+$(document).ready(function () {
+  // TODO1: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
 
+  var saveButton = $(".saveBtn");
+  $(saveButton).on("click", function (e) {
+    // e.preventDefault();
+    var value = $(this).siblings(".description").val();
+    var timeBlockId = $(this).parent().attr("id");
+    console.log(value, timeBlockId)
+    console.log(localStorage)
+    localStorage.setItem(timeBlockId, value);
 
+  });
+  // TODO2: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
 
-// TODO2: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO3: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO4: Add code to display the current date in the header of the page.
+  $('.time-block').each(function () {
+    var blockHour = parseInt($(this).attr('id').split('-')[1]);
+    var currentHour = dayjs().hour()
+    // check if we've moved past this time
+    if (blockHour < currentHour) {
+      $(this).addClass('past');
+    } else if (blockHour === currentHour) {
+      $(this).removeClass('past');
+      $(this).addClass('present');
+    } else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  });
+  //
+  // TODO3: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
 
-// });
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  //
+  // TODO4: Add code to display the current date in the header of the page.
 
-//add this code under TODO1
+  var currentDayEl = $("#currentDay")
 
-var saveButton = $(".saveBtn");
-$(saveButton).on("click", function (e) {
-  // e.preventDefault();
-  var value = $(this).siblings(".description").val();
-  var timeBlockId = $(this).parent().attr("id");
-  console.log(value, timeBlockId)
-  console.log(localStorage)
-  localStorage.setItem(timeBlockId, value);
-
-});
-//end of TODO1
-
-//add this code under TODO2
-// loop over time blocks
-
-$('.time-block').each(function () {
-  var blockHour = parseInt($(this).attr('id').split('-')[1]);
-  var currentHour = dayjs().hour()
-  // check if we've moved past this time
-  if (blockHour < currentHour) {
-    $(this).addClass('past');
-  } else if (blockHour === currentHour) {
-    $(this).removeClass('past');
-    $(this).addClass('present');
-  } else {
-    $(this).removeClass('past');
-    $(this).removeClass('present');
-    $(this).addClass('future');
-  }
-});
-//end of TODO2
-
-//add this code under TODO3
-// var tasks = localStorage.getItem("saveButton")
-// var textArea = document.querySelector(".description")
-// function saveTasks(){
-//   textArea.textContent = tasks
-// }
-
-$(".description").each(function (){
-  var tasks = localStorage.getItem("saveButton")
-  var textArea = document.querySelector(".description")
-  if (localStorage.getItem("saveButton")===null){
-    localStorage.setItem("saveButton",JSON.stringify([]));
-  }
-  textArea.textContent = tasks;
+  function displayDate() {
+    var today = dayjs();
+    currentDayEl.text(today.format("MMM D, YYYY, h:mm:ss"));
+  };
+  setInterval(displayDate, 1000);
 
 });
-
-
-//add this code under TODO4
-var currentDayEl = $("#currentDay")
-
-function displayDate() {
-  var today = dayjs();
-  currentDayEl.text(today.format("MMM D, YYYY, h:mm:ss"));
-};
-setInterval(displayDate, 1000);
-//end of TODO4 code
